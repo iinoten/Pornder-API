0dd950e26f3c182d24e4adefadc0178ade2a935b
Iino Ten <nteten0313@icloud.com>
2019-12-04 18:54:15 +0900

    Add in array

--- index.js
+++ index.js
@@ -14,19 +14,23 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/c/', (req, res) => {
+    let videos_array = []
     const ph = new PornHub();
-    ph.search({category:req.query.category}).then(infos=>{
-      let random_video = infos.videos[Math.floor(Math.random() * infos.videos.length)]
-      var return_infos = {
-        title: random_video.title,
-        video_url: random_video.url,
-        thumb: random_video.thumb,
-        rating: Math.floor(random_video.rating),
-        views: random_video.views,
-      };
-      res.json(return_infos);
-    }).catch(err=>{
-      res.json
-    }) ;
+    {
+      ph.search({category:req.query.category}).then(infos=>{
+        let random_video = infos.videos[Math.floor(Math.random() * infos.videos.length)]
+        var return_infos = {
+          title: random_video.title,
+          video_url: random_video.url,
+          thumb: random_video.thumb,
+          rating: Math.floor(random_video.rating),
+          views: random_video.views,
+        };
+        videos_array.push(return_infos)
+      }).catch(err=>{
+        res.json
+      });
+    }
+    res.json({videos_array});
   }) // 追加
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
