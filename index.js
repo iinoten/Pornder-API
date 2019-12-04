792fd02880a6000d6e2954debd1bf8e56d202f84
Iino Ten <nteten0313@icloud.com>
2019-12-04 18:28:58 +0900

    Add function

--- index.js
+++ index.js
@@ -3,6 +3,20 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 const PornHub = require('@bowwow/pornhub_api');
 
+function get_video_data(category){
+  const ph = new PornHub();
+    ph.search({category:category}).then(infos=>{
+      let random_video = infos.videos[Math.floor(Math.random() * infos.videos.length)]
+      var return_infos = {
+        title: random_video.title,
+        video_url: random_video.url,
+        thumb: random_video.thumb,
+        rating: Math.floor(random_video.rating),
+        views: random_video.views,
+      };
+  })
+}
+
 express()
   .use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*")
@@ -13,19 +27,6 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/c/', (req, res) => {
-    const ph = new PornHub();
-    ph.search({category:req.query.genre}).then(infos=>{
-      let random_video = infos.videos[Math.floor(Math.random() * infos.videos.length)]
-      var return_infos = {
-        title: random_video.title,
-        video_url: random_video.url,
-        thumb: random_video.thumb,
-        rating: Math.floor(random_video.rating),
-        views: random_video.views,
-      };
-      res.json(return_infos);
-    }).catch(err=>{
-      res.json
-    }) ;
-  }) // 追加
+      res.json(get_video_data(req.query.category));
+    })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
