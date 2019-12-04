448f1f02875a3b82d9014f8ac54a32f3486cd349
Iino Ten <nteten0313@icloud.com>
2019-12-04 13:04:09 +0900

    add category

--- index.js
+++ index.js
@@ -8,12 +8,17 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/g/', (req, res) => {
+  .get('/c/', (req, res) => {
     const ph = new PornHub();
     ph.search({category:req.query.genre}).then(infos=>{
-      console.log(infos.videos[0].title)
-      var ppap = infos.videos[0].title;
-      res.json(ppap);
+      let random_video = infos.videos[Math.floor(Math.random() * infos.videos.length)]
+      var return_infos = {
+        title: random_video.title,
+        url: random_video.url,
+        thumbnail: random_video.thumb,
+        rating: Math.floor(random_video.rating)
+      };
+      res.json(return_infos);
     }).catch(err=>{
       res.json
     }) ;
