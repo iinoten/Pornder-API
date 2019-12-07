5e5412ce0d6ef78a5691c219fe473ceb595471ee
Iino Ten <nteten0313@icloud.com>
2019-12-08 04:17:35 +0900

    test

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ express()
     ph.search({category:req.query.category}).then(infos=>{
       let random_video = infos.videos[Math.floor(Math.random() * infos.videos.length)]
       
-      let return_info = random_video.categories[0]
+      let return_info = random_video.categories[0].values()
       res.json(return_info)
     }).catch(err=>{
       res.json
